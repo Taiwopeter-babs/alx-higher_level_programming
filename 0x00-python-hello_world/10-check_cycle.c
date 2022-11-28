a3f77e8387e9af3faadb9787c32a3ea9f9ddf2d5
Taiwo Babalola <babalolataiwop@gmail.com>
2022-11-28 20:41:31 +0100

    inspect cycle in singly linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks for occurence of cycle in linked list
+ * @list: pointer to head of singly linked list
+ * Return: 0 if no cycle, otherwise 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *once;
+	listint_t *twice;
+
+	once = list;
+	twice = list;
+
+	while (once != NULL && twice != NULL)
+	{
+		once = once->next;
+		twice = (twice->next)->next;
+
+		if (twice == once)
+			return (1);
+	}
+
+	return (0);
+}

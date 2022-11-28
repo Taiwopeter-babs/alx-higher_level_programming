88b0abfbcc3bfed2ec08f5bcb61b9ccf2bfca274
Taiwo Babalola <babalolataiwop@gmail.com>
2022-11-28 13:19:26 +0100

    Push new README

---
+++ 0x00-python-hello_world/push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter file name: "
+read file_name
+git add $file_name
+
+echo "Enter commit message: "
+read commit_message
+git commit -m "$commit_message"
+
+git push
+
+read

a1a19cdd6c594d9c3cd33ed3f8a30c08bfd5d3dc
Taiwo Babalola <babalolataiwop@gmail.com>
2022-11-29 16:20:31 +0100

    [ADD] condition for zero

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,12 +13,15 @@ if (num < 0):
     result = num % 10
     print(f"{string} -{result} and is less than 6 and not 0")
 
-
 elif (num > 0):
     string = f"Last digit of {num} is"
+    result = num % 10
     if (result > 5):
         print(f"{string} {result} and is greater than 5")
     elif (result < 6 and result > 0):
         print(f"{string} {result} and is less than 6 and not 0")
     elif (result == 0):
-        print(f"{string} and is 0")
+        print(f"{string} is 0 and is 0")
+
+elif (num == 0):
+    print("Last digit of 0 is 0 and is 0")

8ca7a69e480ed5ad42db0008a59f182897cb6c91
Taiwo Babalola <babalolataiwop@gmail.com>
2022-11-30 15:51:29 +0100

    [ADD] conditions for insert at beginning

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,6 +12,8 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new_node;
 	listint_t *less;
 	listint_t *great;
+	listint_t *equal;
+	listint_t *negative;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
@@ -28,25 +30,54 @@ listint_t *insert_node(listint_t **head, int number)
 		return (new_node);
 	}
 
-	less = *head;
-	great = *head;
-
-	while (less->next != NULL)
+	if (*head)
 	{
-		if (less->n < new_node->n && ((less->next)->n > new_node->n))
-			break;
-		less = less->next;
-	}
+		if (*head->n > new_node->n)
+		{
+			new_node->next = *head;
+			*head = new_node;
 
-	while (great->next != NULL)
-	{
-		if (great->n > new_node->n)
-			break;
-		great = great->next;
-	}
+			return (new_node);
+		}
+		
+		if (*head->n < new_node->n)
+		{
+			less = *head;
+			great = *head;
 
-	less->next = new_node;
-	new_node->next = great;
+			while (less != NULL)
+			{
+				if (less->n < new_node->n && ((less->next)->n > new_node->n))
+					break;
+				less = less->next;
+			}
+
+			while (great != NULL)
+			{
+				if (great->n > new_node->n)
+					break;
+				great = great->next;
+			}
+	
+			equal = *head;
+
+			while (equal->next != NULL)
+			{
+				if (equal->n == new_node->n && (equal->next)->n > new_node->n)
+				{
+					new_node->next = equal->next;
+					equal = new_node;
+					return (new_node);
+				}
+				equal = equal->next;
+			}
+
+
+
+			less->next = new_node;
+			new_node->next = great;
+		}
+	}
 
 	return (new_node);
 }

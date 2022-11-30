3bd32300bc48141c6c42c0ca0ea1f421797fec61
Taiwo Babalola <babalolataiwop@gmail.com>
2022-11-30 19:06:34 +0100

    [ADD] condition for end node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -20,23 +20,32 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 	}
 	new_node->n = number;
-
+	
+	/* if list is empty */
 	if (*head == NULL)
 	{
 		new_node->next = NULL;
 		*head = new_node;
 		return (new_node);
 	}
-
+	
+	/* if list is not empty */
 	if (*head)
 	{
+		/**
+		 * if data in first node is greater than data in new node
+		 * then in a sorted list, int in new node will be the lowest,
+		 * it is added at the beginning of the list
+		 */
 		if ((*head)->n > new_node->n)
 		{
 			new_node->next = *head;
 			*head = new_node;
 			return (new_node);
 		}
-		
+		/**
+		 * if data in first node is lesser than data in new node
+		 */
 		if ((*head)->n < new_node->n)
 		{
 			less = *head;
@@ -44,23 +53,31 @@ listint_t *insert_node(listint_t **head, int number)
 
 			while (less->next != NULL)
 			{
+				/* add node in between the list */
 				if (less->n < new_node->n && ((less->next)->n > new_node->n))
 					break;
+
+				/* add node which has value equal to a node in the list */
 				if (less->n == new_node->n && ((less->next)->n > new_node->n))
 				{
 					new_node->next = less->next;
 					less->next = new_node;
 					return (new_node);
 				}
+				/**
+				 * add node at the end; data in new node is greater
+				 * than all values in the list
+				 */
 				else if (less->n < new_node->n && less->next == NULL)
 				{
-					new_node->next = NULL;
+				 	new_node->next = NULL;
 					less->next = new_node;
 					return (new_node);
 				}
 				less = less->next;
 			}
-
+			
+			/* add node with lower data value in between linked list */
 			while (great != NULL)
 			{
 				if (great->n > new_node->n)
@@ -68,7 +85,7 @@ listint_t *insert_node(listint_t **head, int number)
 				great = great->next;
 			}
 	
-
+			/* set node->next pointers */
 			less->next = new_node;
 			new_node->next = great;
 		}

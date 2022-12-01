bfbd21a50196e71759153c769e26cf9515b4a56d
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-01 17:19:22 +0100

    Build calculator and test

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-from sys import argv
+import sys
 
 
-def tee_calc():
-    operations = {"+": add, "-": sub, "*": mul, "/": div}
-
-    length = len(argv) - 1
+def arithmetic():
+    operators = ["+", "-", "*", "/"]
+    functions = [add, sub, mul, div]
+    
+    length = len(sys.argv) - 1
     if (length != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    operator = argv[2]
-    arg1 = int(argv[1])
-    arg2 = int(argv[3])
-
-    if (operator not in operations):
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
-    result = operations[operator](arg1, arg2)
-    print("{} {} {} = {}".format(arg1, operator, arg2, result))
+    arg1 = int(sys.argv[1])
+    arg2 = int(sys.argv[3])
+    
+    i = 0
+    while (i < len(operators)):
+        if (sys.argv[2] == operators[i]):
+            res = functions[i](arg1, arg2)
+            print("{} {} {} = {}".format(arg1, operators[i], arg2, res))
+            exit(0)
+        i += 1
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
 
 
 if __name__ == "__main__":
-    tee_calc()
+    arithmetic()

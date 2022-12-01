b44c2f4ce7828925eb2a6c2f1c473b4c83227f84
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-01 17:23:07 +0100

    Test calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-import sys
+from sys import argv
 
 
 def arithmetic():
     operators = ["+", "-", "*", "/"]
     functions = [add, sub, mul, div]
-    
-    length = len(sys.argv) - 1
+
+    length = len(argv) - 1
     if (length != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    arg1 = int(sys.argv[1])
-    arg2 = int(sys.argv[3])
-    
+    arg1 = int(argv[1])
+    arg2 = int(argv[3])
+
     i = 0
     while (i < len(operators)):
-        if (sys.argv[2] == operators[i]):
+        if (argv[2] == operators[i]):
             res = functions[i](arg1, arg2)
             print("{} {} {} = {}".format(arg1, operators[i], arg2, res))
             exit(0)

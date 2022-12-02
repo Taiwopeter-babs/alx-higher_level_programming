f78fa812253f028eda046b9e8b2bc4b4154d24d1
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-02 20:29:12 +0100

    Delete element at a specific index in a list

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def delete_at(my_list=[], idx=0):
+    list_len = len(my_list)
+
+    if (idx < 0 or idx > list_len):
+        return (my_list)
+
+    new = my_list
+
+    del new[idx]
+
+    return (new)

461a60504c2c680883f3a0b37c9237255274825f
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-02 11:02:54 +0100

    create new list for replaceable element at index

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
+    new_list = []
+
+    if (idx < 0 or idx > length):
+        new_list += my_list
+        return (new_list)
+
+    new_list += my_list
+    new_list[idx] = element
+
+    return (new_list)

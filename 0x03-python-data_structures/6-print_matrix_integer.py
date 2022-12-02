d87baac6f6f3a07375547f866b4f269130ff6bd5
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-02 12:08:36 +0100

    print elements of a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+
+    if (matrix is None):
+        print()
+        return (None)
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if (j == len(matrix[i]) - 1):
+                print("{}".format(matrix[i][j]))
+                break
+            print("{}".format(matrix[i][j]), end=' ')

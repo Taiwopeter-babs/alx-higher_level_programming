af3440cd348db519680c8f6ed92b2e7d912c8ee9
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-02 10:41:26 +0100

    replace element at index in a list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def replace_in_list(my_list, idx, element):
+    length = len(my_list)
+
+    if (idx < 0 or idx > length):
+        return (my_list)
+
+    my_list[idx] = element
+
+    return (my_list)

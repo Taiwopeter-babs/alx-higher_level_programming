2cc1da0758ec7d5b6777f61d298e3e9b92abefb7
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-07 15:22:36 +0100

    Correct lines in file

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-
 def multiply_list_map(my_list=[], number=0):
-    multiply_by2 = [] + list(map(lambda val: val * number, my_list))
-    return (multiply_by2)
+    return (list(map(lambda val: val * number, my_list)))

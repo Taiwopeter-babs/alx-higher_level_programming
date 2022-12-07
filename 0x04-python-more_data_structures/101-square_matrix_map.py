d063902f832a6648b864ea18838d425c39ffd150
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-07 21:55:26 +0100

    [REPLACE] for loop iterator with map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return ([] + [list(map(lambda x: x ** 2, arr)) for arr in matrix])
+    return list(map(lambda matrix: list(map(lambda x: x**2, matrix)), matrix))

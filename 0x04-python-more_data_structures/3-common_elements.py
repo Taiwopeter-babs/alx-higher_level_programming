3363651595fb998601614923216f0ac336a74892
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-07 15:03:04 +0100

    Test edge cases

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,5 +3,8 @@
 
 def common_elements(set_1, set_2):
     if (not set_1 or not set_2):
-        return
+        if (not set_1):
+            return (set_2)
+        else:
+            return (set_2)
     return (set_1 & set_2)

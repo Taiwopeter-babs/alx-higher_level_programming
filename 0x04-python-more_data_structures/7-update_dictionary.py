c8a13d675096e885c5c0eead087125d249354c5e
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-07 15:17:56 +0100

    [ADD] condition for non-existent key

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -5,6 +5,9 @@ def update_dictionary(a_dictionary, key, value):
     if (not a_dictionary):
         return
 
-    a_dictionary[key] = value
+    if (key in a_dictionary):
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
 
     return (a_dictionary)

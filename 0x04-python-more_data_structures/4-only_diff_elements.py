90f1c1815c62575bfc8c301c1a3a5dcf5a8c4a4e
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-07 15:06:59 +0100

    [ADD] condition for empty sets

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,6 +3,9 @@
 
 def only_diff_elements(set_1, set_2):
     if (not set_1 or not set_2):
-        return
+        if (not set_1):
+            return (set_2)
+        else:
+            return (set_1)
 
     return (set_1 ^ set_2)

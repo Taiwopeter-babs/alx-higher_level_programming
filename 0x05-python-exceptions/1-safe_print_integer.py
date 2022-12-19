42b6acbb2e14b84ff4d84e37a473075dd11fdae1
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-19 13:02:23 +0100

    print integer safely

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    """Prints an integer value
+
+
+    Params:
+            value: argument to be printed
+    Return:
+            Boolean:
+                    True if value is an integer and correctly printed
+                    otherwise False
+    """
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        return (False)
+    else:
+        return (True)

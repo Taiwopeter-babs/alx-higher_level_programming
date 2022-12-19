4f1a2eacb49a391f76557e304e1acbb82890b7f5
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-19 21:10:41 +0100

    print Exception error to stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    """Prints an integer value
+
+
+    Params:
+            value: argument to be printed
+    Return:
+            Boolean:
+                    True if value is an integer and correctly printed
+                    otherwise False and prints Exception error to stderr
+    """
+    if (value):
+        try:
+            print("{:d}".format(value))
+
+        except (TypeError, ValueError) as err:
+            print("Exception:", err, file=sys.stderr)
+            return (False)
+
+        else:
+            return (True)
+    else:
+        return (False)

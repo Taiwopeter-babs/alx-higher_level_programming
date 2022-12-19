ecb324f6e163a120943351905d7d78ffae31cbbe
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-19 12:43:21 +0100

    refactor code for try clause within loop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,13 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        for idx, ele in enumerate(my_list):
-            if (idx < x):
-                print(my_list[idx], end='')
-            idx += 1
-        print()
-    except IndexError:
-        return (None)
-    else:
-        return (idx)
+    idx = 0
+
+    while (idx < x):
+        try:
+            print(my_list[idx], end='')
+        except IndexError:
+            break
+        idx += 1
+    print()
+    return (idx)

6080aad440706e1d668125e40fab49a4bfa07eca
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-19 12:51:19 +0100

    [add] comments for docs

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,16 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
 
 def safe_print_list(my_list=[], x=0):
+    """Prints x number of elements in my_list[]
+
+
+    Params:
+            my_list[]: list containing the elements
+            x: number of elements to print
+    Return:
+            number of elements printed
+    """
     idx = 0
 
     while (idx < x):

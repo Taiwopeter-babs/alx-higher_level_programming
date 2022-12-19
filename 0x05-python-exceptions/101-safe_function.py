b1cca3163c7541293667afe780e4d53c8793ca7f
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-19 21:51:46 +0100

    execute function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    """Executes a function, safely.
+
+
+    Params:
+            fct: pointer to function to be executed
+            args: variable number of arguments to fct
+    Return: result of the function, otherwise None and print
+            to stderr the Exception
+    """
+    if (args):
+        try:
+            if not fct(*args):
+                raise Exception
+
+        except (TypeError, ValueError, Exception) as err:
+            print("Exception:", err, file=sys.stderr)
+
+            return (None)
+
+        else:
+            return (fct(*args))
+    else:
+        return (None)

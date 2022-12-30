29b62115a8b060ddf9fc3c182f3395b8b27730de
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-30 21:35:05 +0100

    Update repository

--- 0x07-python-test_driven_development/3-main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-say_my_name = __import__('3-say_my_name').say_my_name
-
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
-say_my_name("12", "   me     ")

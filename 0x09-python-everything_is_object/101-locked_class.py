3a26678bea972a49bb657e78a43c59cbffc7aeaf
Taiwo Babalola <babalolataiwop@gmail.com>
2023-01-03 18:22:52 +0100

    [add] module that limits the dynamic creation of attributes of class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    This module contains a class `LockedClass` that limits
+    the dynamic creation of attributes at runtime
+"""
+
+class LockedClass:
+    """
+        Empty class that limits the dynamic creation of
+        attribute to `first_name`
+    """
+    __slots__ = "first_name"

c267530826791fb8e89ce30ebb584b39aa5ec21d
Taiwo Babalola <babalolataiwop@gmail.com>
2023-01-12 14:21:37 +0100

    make tests comform to original script

--- 0x07-python-test_driven_development/tests/comp_unittest.py
+++ 0x07-python-test_driven_development/tests/comp_unittest.py
@@ -26,6 +26,23 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer(self.int_list), 15)
         self.assertEqual(max_integer(self.negative_int), -2)
 
+    def test_for_string(self):
+        """Test for string type in list and string argument"""
+        with self.assertRaises(TypeError):
+            max_integer([4, 8, -2, '2', 6])
+
+        with self.assertRaises(TypeError):
+            max_integer("1234566")
+
+    def test_for_float(self):
+        """Test for float type in list"""
+        with self.assertRaises(TypeError):
+            max_integer([-1, 6, 3.6, 7, 31])
+
+    def test_for_argument_type(self):
+        """Test for argument type in function"""
+        with self.assertRaises(TypeError):
+            max_integer((1, 2, 4, 6, 9))
 
     def test_for_empty_and_single(self):
         """test for empty list and list with one element"""

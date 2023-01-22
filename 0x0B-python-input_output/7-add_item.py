8ca18973613918f9ef276c2e39dd6495d90fe1e5
Taiwo Babalola <babalolataiwop@gmail.com>
2023-01-22 22:11:23 +0100

    make script functional

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -16,22 +16,27 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-filename = "./add_item.json"
-obj = []
-length = len(sys.argv)
+def save_strings_from_command_line():
+    filename = "./add_item.json"
+    obj = []
+    length = len(sys.argv)
 
-# if file doesn't exist, create it
-if not os.path.exists(filename):
+    # if file doesn't exist, create it
+    if not os.path.exists(filename):
+        save_to_json_file(obj, filename)
+
+    # load the json file into python object
+    obj = load_from_json_file(filename)
+
+    # append command line strings into the list
+    idx = 1
+    while idx < length:
+        obj.append(sys.argv[idx])
+        idx += 1
+
+    # save/dump to json file
     save_to_json_file(obj, filename)
 
-# load the json file into python object
-obj = load_from_json_file(filename)
 
-# append command line strings into the list
-idx = 1
-while idx < length:
-    obj.append(sys.argv[idx])
-    idx += 1
-
-# save/dump to json file
-save_to_json_file(obj, filename)
+if __name__ == "__main__":
+    save_strings_from_command_line()

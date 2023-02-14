2f97d274e800b2657ac89e7b27c3212ec1435daf
Taiwo Babalola <babalolataiwop@gmail.com>
2023-02-14 21:21:49 +0100

    Alter to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- Alter unicode of database, table and column
+-- database
+ALTER DATABASE hbtn_0c_0 COLLATE utf8mb4_unicode_ci;
+
+-- table => `first_table`
+ALTER TABLE hbtn_0c_0.first_table COLLATE utf8mb4_unicode_ci;
+
+-- column => `name`
+ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;

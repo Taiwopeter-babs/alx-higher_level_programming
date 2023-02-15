c66ef1e14b1d0bdaed245953c1709a0aba02d652
Taiwo Babalola <babalolataiwop@gmail.com>
2023-02-15 17:48:19 +0100

    Include NULL

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Query database for tv shows genres and name, include NULL values
+SELECT tvs.title, tsg.genre_id
+FROM tv_shows tvs
+LEFT JOIN tv_show_genres tsg
+ON tvs.id = tsg.show_id
+-- WHERE tsg.genre_id IS NOT NULL
+ORDER BY tvs.title, tsg.genre_id;

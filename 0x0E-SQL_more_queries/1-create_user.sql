37ba19e1a8f7f2247aad290171a2e76e52d4e924
Taiwo Babalola <babalolataiwop@gmail.com>
2023-02-15 13:49:26 +0100

    create user and grant privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creates a user and grants user root privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
+IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';

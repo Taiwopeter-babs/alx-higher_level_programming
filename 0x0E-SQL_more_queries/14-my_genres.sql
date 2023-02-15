6e30fc023738eb984c6f8a26efc1b41a0e76b416
Taiwo Babalola <babalolataiwop@gmail.com>
2023-02-15 20:33:15 +0100

    Use INNER JOIN clause

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- Join three tables and retrieve values
+SELECT tg.name
+ FROM tv_genres tg
+	INNER JOIN tv_show_genres tsg
+		ON tg.id = tsg.genre_id
+	INNER JOIN tv_shows ts
+		ON ts.id = tsg.show_id
+ 	WHERE ts.title = "Dexter"
+ ORDER BY tg.name;

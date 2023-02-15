d82d3a0db606cc008000aac5b19b0aa512fdcb4d
Taiwo Babalola <babalolataiwop@gmail.com>
2023-02-15 20:38:47 +0100

    Retrieve one genre

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- Join three tables and retrieve values
+SELECT ts.title
+ FROM tv_shows ts
+	INNER JOIN tv_show_genres tsg
+		ON ts.id = tsg.show_id
+	INNER JOIN tv_genres tg
+		ON tg.id = tsg.genre_id
+ 	WHERE tg.name = "Comedy"
+ ORDER BY ts.title;

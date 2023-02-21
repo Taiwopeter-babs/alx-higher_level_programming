f870945ad05c620578d3bd134ffb36f7940b3b22
Taiwo Babalola <babalolataiwop@gmail.com>
2023-02-21 10:37:24 +0100

    add pascal traingle lists

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    This module contains a function that
+    returns a list of lists of patterns
+    of the pascal's triangle
+"""
+
+
+def pascal_triangle(n):
+    in_arr = []
+    outer_arr = []
+
+    row = 0
+    while (row < n):
+
+        col = 0
+        while (col <= row):
+
+            if row == 0 or col == 0:
+                pasc_coef = 1
+            else:
+                pasc_coef *= (row - col + 1) / col
+
+            in_arr.append(int(pasc_coef))
+            col += 1
+        outer_arr.append(in_arr)
+        in_arr = []
+        row += 1
+
+    return outer_arr

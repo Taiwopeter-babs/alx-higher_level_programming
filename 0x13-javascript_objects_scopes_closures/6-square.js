a7639482dddef9f5a17b03db6cc951d230a20db8
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-14 17:23:29 +0100

    add Square class and add method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,14 +8,14 @@ module.exports = class Square extends SquareOne {
 
   // method to print with different character
   charPrint (c) {
-    if (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
       const size = this.width;
 
       for (let i = 0; i < size; i++) {
         console.log('C'.repeat(size));
       }
-    } else {
-      super.print();
     }
   }
 };

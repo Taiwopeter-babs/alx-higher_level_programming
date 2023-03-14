012f95dd05eb3f8725400bcff7a34ebc5cdd903f
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-14 15:23:06 +0100

    Create Rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+
+    width;
+    height;
+
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+    }
+}
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,7 @@
+# Javascript - Objects, Scopes and Closures
+
+- Objects
+- JS OOP
+- Classes
+- Binding in JS
+- Prototypes

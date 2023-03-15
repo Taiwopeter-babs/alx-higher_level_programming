a25260670296bd6d4168cd8946296387f80c24af
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-15 01:21:32 +0100

    map function to array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 
-const newList = list.map((item, index) => item * index);
+(function () {
+  const newList = list.map((item, index) => item * index);
 
-console.log(list);
-console.log(newList);
+  console.log(list);
+  console.log(newList);
+})();

5dcdff3c24e16a971660491cb69e8c443b3d632a
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-15 14:19:28 +0100

    create new object with derived properties

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+
+// get an array of unique values from the dict object
+const newArray = Object.values(dict).filter((value, index) => {
+  return Object.values(dict).indexOf(value) === index;
+});
+
+// create a new object
+const newObject = {};
+
+/*
+ * create a new array for each value of Array newArray that holds
+ * the keys with values matching the values in newArray
+ */
+for (let i = 0; i < newArray.length; i++) {
+  const objValueArray = Object.keys(dict).filter((k) => dict[k] === newArray[i]);
+  newObject[newArray[i]] = objValueArray;
+}
+console.log(newObject);

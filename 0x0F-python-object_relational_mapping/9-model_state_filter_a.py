a296580f81d25bf0913dd69c47401eb593ebb371
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-16 21:23:28 +0100

    Add filter to query results

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This module contains a function that fetches data from a table"""
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+def filter_query():
+    """Queries the first of State objects"""
+    # Arguments from terminal
+    user = argv[1]
+    pwd = argv[2]
+    database = argv[3]
+
+    """
+        The engine and database schema are created
+        then engine is bound to current session with session maker
+    """
+    URL = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+    engine = create_engine(URL.format(user, pwd, database))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    # creates a new session
+    session = Session()
+
+    # Query of filtered result
+    query_states = session.query(State).filter(State.name.like("%a%"))
+
+    # Add order to query result
+    for state in query_states.order_by(State.id.asc()).all():
+        print("{}: {}".format(state.id, state.name))
+
+    # Cleanup
+    session.commit()
+    session.close()
+
+
+if __name__ == "__main__":
+    filter_query()

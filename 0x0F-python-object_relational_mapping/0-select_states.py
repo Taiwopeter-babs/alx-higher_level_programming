c54829c509c4b1f34711e6ef475a71cb8b42b0b5
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-16 11:14:57 +0100

    Retrieve data from database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module retrieves data from database"""
+from sys import argv
+import MySQLdb
+
+HOST = "localhost"
+PORT = 3306
+USER = argv[1]
+PASSWD = argv[2]
+D_BASE = argv[3]
+print(HOST, USER, PASSWD, D_BASE)
+
+db = MySQLdb.connect(
+    host=HOST, port=PORT, user=USER, passwd=PASSWD, db=D_BASE, charset="utf8"
+)
+cur = db.cursor()
+
+cur.execute("SELECT id, name FROM states")
+
+all_rows = cur.fetchall()
+for row in all_rows:
+    print(row)
+
+cur.close()
+db.close()

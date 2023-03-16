26665e2c825e3cf259a81a1ed3c98ea8e7decf01
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-16 16:12:29 +0100

    query with ORM

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines a mapped class that classes
+    inherit from and a State class
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+# Mapper class
+Base = declarative_base()
+
+
+class State(Base):
+    """Definition of State Class"""
+
+    __tablename__ = "states"
+
+    id = Column(
+        Integer, autoincrement=True, primary_key=True, nullable=False, unique=True
+    )
+    name = Column(String(128), nullable=False)
--- 0x0F-python-object_relational_mapping/temp.py
+++ 0x0F-python-object_relational_mapping/temp.py
@@ -4,23 +4,28 @@ from sys import argv
 import MySQLdb
 
 
-def query_with_safe_filter():
-    """Get filter results returned to object"""
+def query_multiple_tables():
+    """Return results to object"""
     HOST = "localhost"
     PORT = 3306
     USER = argv[1]
     PWD = argv[2]
     D_BASE = argv[3]
-    arg = arg[4]
 
     db = MySQLdb.connect(
         host=HOST, port=PORT, user=USER, passwd=PWD, db=D_BASE, charset="utf8"
     )
     cur = db.cursor()
 
-    """Using this format prevents SQL INJECTION"""
-    sql_cmd = "SELECT id, name FROM states WHERE name=%s ORDER BY id"
-    cur.execute(sql_cmd, (arg,))
+    # SQL command
+    sql_cmd = (
+        "SELECT c.id, c.name, st.name "
+        "FROM cities c "
+        "LEFT JOIN states st "
+        "ON c.state_id = st.id ORDER BY c.id"
+    )
+    print(sql_cmd)
+    cur.execute(sql_cmd)
 
     all_rows = cur.fetchall()
     for row in all_rows:
@@ -31,4 +36,4 @@ def query_with_safe_filter():
 
 
 if __name__ == "__main__":
-    query_with_safe_filter()
+    query_multiple_tables()

548a10ae9d933b26f7852483c815de9492249756
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-17 01:11:47 +0100

    Update record

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This module contains a function that fetches data from a table"""
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+def update_object():
+    """Queries the first of State objects"""
+    # Arguments from terminal
+    user = argv[1]
+    pwd = argv[2]
+    database = argv[3]
+
+    """
+        The engine and database schema are created
+        then engine is bound to current session with session maker
+    """
+    URL = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+    engine = create_engine(URL.format(user, pwd, database))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    # creates a new session
+    session = Session()
+
+    # Query object and update
+    query_object = session.query(State).filter_by(id=2).first()
+    query_object.name = "New Mexico"
+
+    # Print query result
+    print(query_object.name)
+
+    # Cleanup
+    session.commit()
+    session.close()
+
+
+if __name__ == "__main__":
+    update_object()

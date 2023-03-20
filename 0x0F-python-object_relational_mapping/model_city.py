30a2234255b80f12249fa8f8ea1db0cd5c3f5c33
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-20 18:32:13 +0100

    refactor for code validation

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -11,8 +11,7 @@ class City(Base):
 
     __tablename__ = "cities"
 
-    id = Column(
-        Integer, autoincrement=True, primary_key=True, nullable=False, unique=True
-    )
+    id = Column(Integer, autoincrement=True, primary_key=True,
+                nullable=False, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)

9336b8f000510148a6dfddb6ae0e1a34705ed49a
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-20 18:21:16 +0100

    refactor code for pycodestyle

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -15,7 +15,6 @@ class State(Base):
 
     __tablename__ = "states"
 
-    id = Column(
-        Integer, autoincrement=True, primary_key=True, nullable=False, unique=True
-    )
+    id = Column(Integer, autoincrement=True, primary_key=True,
+                nullable=False, unique=True)
     name = Column(String(128), nullable=False)

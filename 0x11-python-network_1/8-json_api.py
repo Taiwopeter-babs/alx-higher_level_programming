9d5dafd2c4c3fb46a350bd474cb516d24c93dc40
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-31 21:25:26 +0100

    make request from an api and validate response

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This script communicates with an API and gets a JSON formatted value"""
+import requests
+from sys import argv
+
+
+def get_request_status(letter: str):
+    payload = {}
+    payload["q"] = letter
+
+    url = "http://0.0.0.0:5000/search_user"
+    req = requests.post(url, data=payload)
+
+    if isinstance(req.json(), dict):
+        resp_dict = req.json()
+        if resp_dict:
+            print("[{}] {}".format(resp_dict.get("id"), resp_dict.get("name")))
+        else:
+            print("No result")
+    else:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    try:
+        get_request_status(argv[1])
+    except IndexError:
+        get_request_status("")

ac89a74d38ece8c02c4c9e43f2c07d3c669cf5bc
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-31 12:05:58 +0100

    get response from url and decode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ def get_body_decode():
         req = Request("https://alx-intranet.hbtn.io/status")
         with urlopen(req) as response:
             body = response.read()
-        print("Body Response:")
+        print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))

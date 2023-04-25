6871ce2611274fe1bfaa1d26ad6f9fe91591ebca
Taiwo Babalola <babalolataiwop@gmail.com>
2023-04-25 16:34:39 +0100

    update script

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 // This script reads the content of a file from the command line
 const fs = require('fs');
+const filePath = process.argv[2];
 
-(function () {
-  const filePath = process.argv[2];
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log(data);
-  });
-})();
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
+});
